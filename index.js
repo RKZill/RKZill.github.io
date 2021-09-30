88362725793eb2b0c6ac7d938333c555d27cf911
Ryan Kelsey <ryankelsey14@outlook.com>
2021-09-30 16:21:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,4 +29,6 @@ function deselectAll(){
   for (i = 1; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+  document.getElementById(tablinks[0]).style.display = "block";
+  evt.currentTarget.className += " active";
 }
