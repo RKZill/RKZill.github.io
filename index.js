77c925e7ea80a0de315fcf65d976092322de22bf
Ryan Kelsey <ryankelsey14@outlook.com>
2022-06-01 19:13:00 -0400

    Testing jQuery

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+document.writeln("<script src = 'https://code.jquery.com/jquery-3.6.0.min.js' integrity = 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin = 'anonymous' ></script >");
+jQuery(function ($) {
+    $('.hello').hide();
+});
+
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
 
