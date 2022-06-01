0d2cdaee7f5b4dbf5358d486a4a9771e0ef5511d
Ryan Kelsey <ryankelsey14@outlook.com>
2022-06-01 19:17:14 -0400

    Removed error

--- index.js
+++ index.js
@@ -21,23 +21,6 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
-function openTut(evt, tabName) {
-  var i, tabcontent, tablinks;
-
-  tabcontent = document.getElementsByClassName("tabcontentTutorial");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinksTutorial");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
 function deselectAll(){
   var tabcontent, tablinks, i;
   tabcontent = document.getElementsByClassName("tabcontent");
