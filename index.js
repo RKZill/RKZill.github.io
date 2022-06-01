8793279d684d5f594b3671c5f92508f1f2b85562
Ryan Kelsey <ryankelsey14@outlook.com>
2022-06-01 19:23:45 -0400

    Removed

--- index.js
+++ index.js
@@ -3,34 +3,3 @@
 jQuery(function ($) {
     $('.hello').hide();
 });
-
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-function deselectAll(){
-  var tabcontent, tablinks, i;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 1; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 1; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tablinks[0]).style.display = "block";
-  evt.currentTarget.className += " active";
-}
