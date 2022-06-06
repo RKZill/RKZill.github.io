52c6fa6d058a396a5c971abf7eb8007e710d4554
Ryan Kelsey <ryankelsey14@outlook.com>
2022-06-05 23:28:23 -0400

    Redesigned modals

--- index.js
+++ index.js
@@ -2,10 +2,4 @@
 
 jQuery(function ($) {
     $(document).foundation();
-    console.log('closed');
-    $('#welcome_reveal').foundation('open');
-    console.log('opened');
-    
 });
-
-console.log("HELLO THERE2");
